63da262c2267e798f6b2cdda3ad8b8ad738ed359
Saad <saadwaqar118@gmail.com>
2025-01-14 02:43:30 +0500

    Model trained

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n-cls.pt')  # load the model
 
+model.train(data='/home/saad_waqar/PycharmProjects/Weather_classification/dataset'
+            , epochs=1, imgsz=64)
